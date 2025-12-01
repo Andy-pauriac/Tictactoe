a2d04d8b97916dfea00a5fd240d49af2784eb568
Andy pauriac <andy.pauriac@laplateforme.io>
2025-12-01 15:23:20 +0100

    MAJ

--- morpion.py
+++ morpion.py
@@ -1,5 +1,21 @@
-def draw_board():
-    board = "|1|2|3|\n|4|5|6|\n|7|8|9|"
-    print(board)
+from helpers import draw_board , check_turn
+import os
 
+spots = {1 : "1" , 2 : "2", 3 : "3", 4 : "4", 5 : "5",
+         6 : "6", 7 : "7", 8 : "8", 9 : "9"}
 
+playing = True
+turn = 0
+
+while playing:
+    #reset the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    draw_board(spots)
+    
+    choice = input()
+    if choice == 'q':
+        playing = False
+    elif str isdigit(choice) and int(choice) in spots:
+        if not spots [int(choice)] in ['X' , 'O']:
+            turn += 1
+            spots[int(choice)] = check_turn(turn)
