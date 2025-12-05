a09fccf3dd62618dacd61c0c460debc06966b744
Andylaplateforme <andy.pauriac@laplateforme.io>
2025-12-05 09:44:56 +0100

    update file for the projet tictactoe

--- menu.py
+++ menu.py
@@ -3,7 +3,6 @@ def main_menu(play_pvp_callback):
         print("\n=== MENU ===")
         print("1. Jouer contre un autre joueur")
         print("2. Jouer contre l'ordinateur")
-        # print("3. Rules")
         print("3. Quitter")
 
         choice = input("choisis une option (1-3): ")
@@ -15,9 +14,6 @@ def main_menu(play_pvp_callback):
             print("MODE NON INPLANTÉ")
             input("ENTRER pour continuer")
 
-        # elif choice == "3":
-        #     show_rules()
-
         elif choice == "3":
             print("AU REVOIR")
             break
@@ -26,10 +22,3 @@ def main_menu(play_pvp_callback):
             print("Entré invalide , veuillez réessayer .")
 
 
-# def show_rules():
-#     print("\n--- RULES ---")
-#     print("1. The game is played on a 3x3 grid.")
-#     print("2. Player 1 is X, Player 2 or AI is O.")
-#     print("3. First to get 3 in a row wins!")
-#     print("4. If the grid is full = draw")
-#     input("\nPress ENTER to return...")
