4e0250c35a8e5d4631653d1196d8a8910a905e81
Andylaplateforme <andy.pauriac@laplateforme.io>
2025-12-08 08:52:48 +0100

    update file for the projet tictactoe

--- morpion.py
+++ morpion.py
@@ -1,51 +1,50 @@
 from helpers import draw_board , check_turn , check_for_win
-import os
-import random
-import sys
+import os #Permet d'interagir avec le système exploitation (exemple : effacer le contenu a l'écran )
+import random #Utiliser pour que l'ordinateur choissent une case au hasard 
 
 ########################################
 #     PARTIE JOUEUR VS JOUEUR
 ########################################
 def play_pvp():
-    spots = {i:str(i) for i in range(1,10)}
-    playing = True
-    complete = False
-    turn = 0
-    prev_turn = -1
+    spots = {i:str(i) for i in range(1,10)} #crée un dictionnaire qui represente les 9 cases du jeu (vide ))
+    playing = True # maintient la boucle de jeu active
+    complete = False #False --> True si quelqu'un gagne
+    turn = 0 #compteur de tours 
+    prev_turn = -1 #permet de détecter si le joueur a entrer une case invalide
 
     while playing:
-        os.system('cls' if os.name=='nt' else 'clear')
-        draw_board(spots)
+        os.system('cls' if os.name=='nt' else 'clear') #permet de garder l'affichage propres en effaçant le contenu sur l'écran
+        draw_board(spots) #affiche la grille actuel
 
         if prev_turn == turn:
             print("Case déjà prise, rejouez.")
-        prev_turn = turn
+        prev_turn = turn 
 
         print("Joueur " + str((turn %2)+1) + " choisit une case (ou 'q' pour quitter) :")
-        choice = input()
+        choice = input() #demande au joueur (1 ou 2) d'entrer une case choisit
 
         if choice == 'q':
             playing = False
-            break
+            break #permet d'arreter le jeu
 
-        if choice.isdigit() and int(choice) in spots:
-            if spots[int(choice)] not in ["X","O"]:
-                turn += 1
-                spots[int(choice)] = check_turn(turn)
-            else:
-                prev_turn = turn
+        if choice.isdigit() and int(choice) in spots: #verifie si l'entrée est un chiffre et que ce chiffre est bien compris dans la liste spots
+            if spots[int(choice)] not in ["X","O"]: #vérifie si la case n'est pas déja occupé par un X ou un O 
+                turn += 1 #passe au tour suivant 
+                spots[int(choice)] = check_turn(turn) #on marque la case avec le signe choisit
+            else: 
+                prev_turn = turn #si la case est déja prise , on ne passe pas au tour suivant 
 
-        if check_for_win(spots): playing, complete = False, True
-        if turn >= 9: playing = False
+        if check_for_win(spots): playing, complete = False, True #vérifie si un des joueurs a aligné 3 même symbole
+        if turn >= 9: playing = False # Si le nombre de tour dépasse 9 , on déclenche un match nul
 
     os.system('cls' if os.name=='nt' else 'clear')
     draw_board(spots)
 
     if complete:
-        if check_turn(turn) == "X": print("Félicitations ! Joueur 1 a gagné !")
-        else: print("Félicitations ! Joueur 2 a gagné !")
+        if check_turn(turn) == "X": print("Félicitations ! Joueur 1 a gagné !") #si trois X sont alignés , le joueur 1 gagne
+        else: print("Félicitations ! Joueur 2 a gagné !") #si trois O sont alignés , le joueur 2 gagne
     else:
-        print("Égalité !")
+        print("Égalité !") # au bout du 9eme tour , si aucun des joueurs n'as aligné 3 symboles , c'est égalité
 
     input("\nPress ENTER pour revenir au menu...")
 
@@ -53,24 +52,24 @@ def play_pvp():
 # JOUEUR CONTRE ORDINATEUR
 ########################################
 def ordinateur():
-    spots = {i:str(i) for i in range(1,10)}
+    spots = {i:str(i) for i in range(1,10)} 
     playing = True
-    complete = False
-    turn = 0
+    complete = False 
+    turn = 0 
     prev_turn = -1
 
-    os.system('cls' if os.name=='nt' else 'clear')
-    print("Vous êtes Joueur 1 (X)")
-    print("L'IA est Joueur 2 (O)")
-    input("Appuyez sur ENTER pour commencer...")
+    os.system('cls' if os.name=='nt' else 'clear')  
+    print("Vous êtes Joueur 1 (X)") #informe que le joueur 1 possède le symbole X 
+    print("L'IA est Joueur 2 (O)") #informe que l'ordinateur possède le symbole O
+    input("Appuyez sur ENTER pour commencer...") 
 
     while playing:
-        os.system('cls' if os.name=='nt' else 'clear')
-        draw_board(spots)
+        os.system('cls' if os.name=='nt' else 'clear') 
+        draw_board(spots) 
 
         # Tour du joueur humain
-        if turn % 2 == 0:
-            if prev_turn == turn:
+        if turn % 2 == 0: #si le tour est pair ( 0 , 2 , 4 ) alors ça signie que c'est le tour du joueur 1
+            if prev_turn == turn: 
                 print("Case déjà prise, rejouez.")
             prev_turn = turn
 
@@ -79,10 +78,10 @@ def ordinateur():
 
             if choice == 'q': playing = False; break
 
-            if choice.isdigit() and int(choice) in spots:
-                if spots[int(choice)] not in ["X","O"]:
-                    turn += 1
-                    spots[int(choice)] = "X"
+            if choice.isdigit() and int(choice) in spots: 
+                if spots[int(choice)] not in ["X","O"]: #vérifie si la case n'est pas déja occupé par un X ou un O 
+                    turn += 1 
+                    spots[int(choice)] = "X" # permet de remplacer la case par un X 
                 else:
                     prev_turn = turn
             else:
@@ -97,7 +96,7 @@ def ordinateur():
 
             ai_choice = random.choice(empty)
             print(f"L'IA choisit la case {ai_choice}...")
-            spots[ai_choice] = "O"
+            spots[ai_choice] = "O" #remplace la case choisit par l'ordinateur par un O 
             turn += 1
 
         # Vérification victoire
